e9e239b9506ddd3e2ed7d13216d48c9e7eca224e
Adam Patridge <patridge@gmail.com>
2017-09-26 17:01:37 -0600

    Fixed an extension method name/usage.

--- Exercise 4/Start/MyTunes.NetStandard/SongNameExtensions.cs
+++ Exercise 4/Start/MyTunes.NetStandard/SongNameExtensions.cs
@@ -6,7 +6,7 @@ namespace MyTunes.NetStandard
     public static class SongNameExtensions
     {
         public static HttpClient httpClient = new HttpClient();
-        public static string MessUpName(this string songName)
+        public static string RuinSongName(this string songName)
         {
             return songName.Replace("Crocodile", "Alligator");
         }
